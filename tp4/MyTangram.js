b786b58ab2e039baaceed4389b23f5b103d124cc
Nuno Pereira <nunoafonso2002@gmail.com>
2023-03-14 11:47:38 +0000

    Added more files

---
+++ tp4/MyTangram.js
@@ -0,0 +1,72 @@
+import { CGFobject } from '../lib/CGF.js';
+import { MyDiamond } from './MyDiamond.js';
+import { MyParallelogram } from './MyParallelogram.js';
+import { MyTriangle } from './MyTriangle.js';
+import { MyTriangleBig } from './MyTriangleBig.js';
+import { MyTriangleSmall } from './MyTriangleSmall.js';
+
+/**
+ * MyTriangle
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyTangram extends CGFobject {
+    constructor(scene) {
+        super(scene);
+
+        //Initialize objects
+        this.diamond = new MyDiamond(scene);
+        this.triangle = new MyTriangle(scene);
+        this.parallelogram = new MyParallelogram(scene);
+        this.triangleSmall = new MyTriangleSmall(scene);
+        this.triangleBig = new MyTriangleBig(scene);
+    }
+
+    display() {
+        this.scene.pushMatrix()
+        this.scene.multMatrix([
+            1,  0,  0,  0,
+            0,  1,  0,  0,
+            0,  0,  1,  0,
+            -1, 0,  0,  1,
+        ]);
+        this.diamond.display()
+        this.scene.popMatrix()
+
+        this.scene.pushMatrix()
+        this.scene.rotate(Math.PI, 0, 1, 0);
+        this.scene.rotate(Math.PI/3, 0, 0, 1);
+        this.parallelogram.display()
+        this.scene.popMatrix()
+
+        this.scene.pushMatrix()
+        this.scene.translate(1, 0, 0);
+        this.triangleSmall.display()
+        this.scene.popMatrix()
+
+        this.scene.pushMatrix()
+        this.scene.translate(-2, -1, 0);
+        this.scene.rotate(-Math.PI/2, 0, 0, 1);
+        this.triangleSmall.display()
+        this.scene.popMatrix()
+
+        this.scene.pushMatrix()
+        this.scene.translate(1, -1, 0);
+        this.scene.rotate(Math.PI, 0, 0, 1);
+        this.triangle.display()
+        this.scene.popMatrix()
+
+        this.scene.pushMatrix()
+        this.scene.translate(0, -2, 0);
+        this.scene.rotate(-Math.PI/2, 0, 0, 1);
+        this.triangleBig.display()
+        this.scene.popMatrix()
+
+        this.scene.pushMatrix()
+        this.scene.translate(0, -2, 0);
+        this.scene.rotate(Math.PI/2, 0, 0, 1);
+        this.triangleBig.display()
+        this.scene.popMatrix()
+    }
+}
+
---
+++ tp4/MyTriangleBig.js
@@ -0,0 +1,32 @@
+import {CGFobject} from '../lib/CGF.js';
+/**
+ * MyTriangleBig
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+export class MyTriangleBig extends CGFobject {
+    constructor(scene) {
+        super(scene);
+        this.initBuffers();
+    }
+    
+    initBuffers() {
+        this.vertices = [
+            0, 2, 0,   // A
+            -2, 0, 0,  // B
+            2, 0, 0,   // C
+        ];
+
+        //Counter-clockwise reference of vertices
+        this.indices = [
+            0, 1, 2,    // A B C
+        ];
+
+        //The defined indices (and corresponding vertices)
+        //will be read in groups of three to draw triangles
+        this.primitiveType = this.scene.gl.TRIANGLES;
+
+        this.initGLBuffers();
+    }
+}
+
