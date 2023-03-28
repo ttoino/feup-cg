4b985a6e62034006193c6b921ebb84c4019e25eb
toino <joaoapereira21@hotmail.com>
2023-03-28 10:54:17 +0100

    Format project code

--- project/MyScene.js
+++ project/MyScene.js
@@ -1,4 +1,11 @@
-import { CGFscene, CGFcamera, CGFaxis, CGFappearance, CGFshader, CGFtexture } from "../lib/CGF.js";
+import {
+    CGFscene,
+    CGFcamera,
+    CGFaxis,
+    CGFappearance,
+    CGFshader,
+    CGFtexture,
+} from "../lib/CGF.js";
 import { MyPlane } from "./MyPlane.js";
 
 /**
@@ -6,84 +13,88 @@ import { MyPlane } from "./MyPlane.js";
  * @constructor
  */
 export class MyScene extends CGFscene {
-  constructor() {
-    super();
-  }
-  init(application) {
-    super.init(application);
-    
-    this.initCameras();
-    this.initLights();
+    constructor() {
+        super();
+    }
 
-    //Background color
-    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    init(application) {
+        super.init(application);
 
-    this.gl.clearDepth(100.0);
-    this.gl.enable(this.gl.DEPTH_TEST);
-    this.gl.enable(this.gl.CULL_FACE);
-    this.gl.depthFunc(this.gl.LEQUAL);
+        this.initCameras();
+        this.initLights();
 
-    //Initialize scene objects
-    this.axis = new CGFaxis(this);
-    this.plane = new MyPlane(this,30);
+        //Background color
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-    //Objects connected to MyInterface
-    this.displayAxis = true;
-    this.scaleFactor = 1;
+        this.gl.clearDepth(100.0);
+        this.gl.enable(this.gl.DEPTH_TEST);
+        this.gl.enable(this.gl.CULL_FACE);
+        this.gl.depthFunc(this.gl.LEQUAL);
 
-    this.enableTextures(true);
+        //Initialize scene objects
+        this.axis = new CGFaxis(this);
+        this.plane = new MyPlane(this, 30);
 
-this.texture = new CGFtexture(this, "images/terrain.jpg");
-this.appearance = new CGFappearance(this);
-this.appearance.setTexture(this.texture);
-this.appearance.setTextureWrap('REPEAT', 'REPEAT');
+        //Objects connected to MyInterface
+        this.displayAxis = true;
+        this.scaleFactor = 1;
 
-  }
-  initLights() {
-    this.lights[0].setPosition(15, 0, 5, 1);
-    this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
-    this.lights[0].enable();
-    this.lights[0].update();
-  }
-  initCameras() {
-    this.camera = new CGFcamera(
-      1.0,
-      0.1,
-      1000,
-      vec3.fromValues(50, 10, 15),
-      vec3.fromValues(0, 0, 0)
-    );
-  }
-  setDefaultAppearance() {
-    this.setAmbient(0.2, 0.4, 0.8, 1.0);
-    this.setDiffuse(0.2, 0.4, 0.8, 1.0);
-    this.setSpecular(0.2, 0.4, 0.8, 1.0);
-    this.setShininess(10.0);
-  }
-  display() {
-    // ---- BEGIN Background, camera and axis setup
-    // Clear image and depth buffer everytime we update the scene
-    this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
-    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-    // Initialize Model-View matrix as identity (no transformation
-    this.updateProjectionMatrix();
-    this.loadIdentity();
-    // Apply transformations corresponding to the camera position relative to the origin
-    this.applyViewMatrix();
+        this.enableTextures(true);
 
-    // Draw axis
-    if (this.displayAxis) this.axis.display();
+        this.texture = new CGFtexture(this, "images/terrain.jpg");
+        this.appearance = new CGFappearance(this);
+        this.appearance.setTexture(this.texture);
+        this.appearance.setTextureWrap("REPEAT", "REPEAT");
+    }
 
-    // ---- BEGIN Primitive drawing section
+    initLights() {
+        this.lights[0].setPosition(15, 0, 5, 1);
+        this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+        this.lights[0].enable();
+        this.lights[0].update();
+    }
 
-    this.pushMatrix();
-    this.appearance.apply();
-    this.translate(0,-100,0);
-    this.scale(400,400,400);
-    this.rotate(-Math.PI/2.0,1,0,0);
-    this.plane.display();
-    this.popMatrix();
+    initCameras() {
+        this.camera = new CGFcamera(
+            1.0,
+            0.1,
+            1000,
+            vec3.fromValues(50, 10, 15),
+            vec3.fromValues(0, 0, 0)
+        );
+    }
 
-    // ---- END Primitive drawing section
-  }
+    setDefaultAppearance() {
+        this.setAmbient(0.2, 0.4, 0.8, 1.0);
+        this.setDiffuse(0.2, 0.4, 0.8, 1.0);
+        this.setSpecular(0.2, 0.4, 0.8, 1.0);
+        this.setShininess(10.0);
+    }
+
+    display() {
+        // ---- BEGIN Background, camera and axis setup
+        // Clear image and depth buffer everytime we update the scene
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+        // Initialize Model-View matrix as identity (no transformation
+        this.updateProjectionMatrix();
+        this.loadIdentity();
+        // Apply transformations corresponding to the camera position relative to the origin
+        this.applyViewMatrix();
+
+        // Draw axis
+        if (this.displayAxis) this.axis.display();
+
+        // ---- BEGIN Primitive drawing section
+
+        this.pushMatrix();
+        this.appearance.apply();
+        this.translate(0, -100, 0);
+        this.scale(400, 400, 400);
+        this.rotate(-Math.PI / 2.0, 1, 0, 0);
+        this.plane.display();
+        this.popMatrix();
+
+        // ---- END Primitive drawing section
+    }
 }
